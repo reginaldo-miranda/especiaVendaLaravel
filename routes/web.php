6b298962dab8a3bdd029446c5503243bb95bc497
reginaldo <reginaldobrain@gmail.com>
2020-12-28 14:01:14 -0300

    firma 2812 14:01

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// route defaut
+
+//use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+
+//use Illuminate\Routing\Route;
+
+Route::get('/contato', function() {
+    return view('fr_teste');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/welcome');
 });
